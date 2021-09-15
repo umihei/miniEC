dcbed4876818e2f9abb325cbfc44862c716cbf74
umihei <wakitani.kaihei@gmail.com>
2021-09-15 01:15:35 +0000

    use react app

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [{
+            _id: '1',
+            name: 'broccoli',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '2',
+            name: 'carrot',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '3',
+            name: 'onion',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '4',
+            name: 'tomato',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '5',
+            name: 'potato',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '6',
+            name: 'water melon',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 680,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+    ]
+};
+
+export default data;
