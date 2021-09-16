05c0ffd3cb3666b59ce30665d8f17967c9e898e3
wakitani <wakitani@wakitanis-MacBook-Pro.local>
2021-09-16 22:23:32 +0900

    add product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import data from '../data';
+import Rating from '../components/Rating';
 
 export default function ProductScreen(props) {
     console.log('call ProductScreen')
@@ -9,19 +10,62 @@ export default function ProductScreen(props) {
     }
     return (
     <div>
-    <div className="row">
-        <div className="col-2">
-            <img className="large" src={product.image} alt={product.name} ></img>
+        <div className="row top">
+            <div className="col-2">
+                <img className="large" src={product.image} alt={product.name} ></img>
+            </div>
+        
+
+        <div className="col-1">
+            <ul>
+                <li>
+                    <h1>{product.name}</h1>
+                </li>
+                <li>
+                    <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+                </li>
+                <li>
+                    Price : {product.price} yen
+                </li>
+                <li>
+                    Description:
+                    <p>{product.description}</p>
+                </li>
+            </ul>
+        
         </div>
-    </div>
-    <div className="col-1">
-    
-    </div>
-    
-    <div className="col-1">
-    
-    </div>
-            
+        
+        <div className="col-1">
+            <div className="card card-body">
+                <ul>
+                    <li>
+                        <div className="row">
+                            <div>Price</div>
+                            <div className="price">{product.price} yen</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row">
+                            <div>Status</div>
+                            <div>
+                                {product.contInStock > 0 ? (
+                                    <span className="success">In Stock</span>
+                                ) : (
+                                    <span className="error">Unavailable</span>
+                                )
+                                }
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <button className="primary block">Add to Cart</button>
+                    </li>
+                </ul>
+            </div>
+        
+        </div>
+        </div>
+                
     </div>
     )
 }
