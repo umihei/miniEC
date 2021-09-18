6a23e4c652750dad2161e664242ba9350c8aaaac
wakitani <wakitani@wakitanis-MacBook-Pro.local>
2021-09-18 13:06:29 +0900

    get contents from backend, add loading box, message box

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Product from '../components/Product';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
 
 export default function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const { data } = await axios.get('/api/products');
+                setProducts(data);
+                setLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
     console.log('call HomeScreen')
-    return (<div>
-                <div className="row center">
-                {
-                    data.products.map((product) => (
-                    <Product key={product._id} product={product}></Product>
-                    ))
-                }
-                    
-                </div>
-            </div>)
+    return (
+        <div>
+            {loading ? <LoadingBox></LoadingBox>
+                :
+                error ? <MessageBox variant="danger">{error}</MessageBox>
+                    : <div className="row center">
+                        {
+                            products.map((product) => (
+                                <Product key={product._id} product={product}></Product>
+                            ))
+                        }
+
+                    </div>
+            }
+
+        </div>)
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,68 +6,68 @@ import { Link } from 'react-router-dom';
 export default function ProductScreen(props) {
     console.log('call ProductScreen')
     const product = data.products.find(x => x._id === props.match.params.id)
-    if(!product){
+    if (!product) {
         return <div> Product Not Found </div>
     }
     return (
-    <div>
-        <Link to="/">Back to result</Link>
-        <div className="row top">
-            <div className="col-2">
-                <img className="large" src={product.image} alt={product.name} ></img>
-            </div>
-        
+        <div>
+            <Link to="/">Back to result</Link>
+            <div className="row top">
+                <div className="col-2">
+                    <img className="large" src={product.image} alt={product.name} ></img>
+                </div>
 
-        <div className="col-1">
-            <ul>
-                <li>
-                    <h1>{product.name}</h1>
-                </li>
-                <li>
-                    <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
-                </li>
-                <li>
-                    Price : {product.price} yen
-                </li>
-                <li>
-                    Description:
-                    <p>{product.description}</p>
-                </li>
-            </ul>
-        
-        </div>
-        
-        <div className="col-1">
-            <div className="card card-body">
-                <ul>
-                    <li>
-                        <div className="row">
-                            <div>Price</div>
-                            <div className="price">{product.price} yen</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row">
-                            <div>Status</div>
-                            <div>
-                                {product.contInStock > 0 ? (
-                                    <span className="success">In Stock</span>
-                                ) : (
-                                    <span className="error">Unavailable</span>
-                                )
-                                }
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <button className="primary block">Add to Cart</button>
-                    </li>
-                </ul>
+
+                <div className="col-1">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+                        </li>
+                        <li>
+                            Price : {product.price} yen
+                        </li>
+                        <li>
+                            Description:
+                            <p>{product.description}</p>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div className="col-1">
+                    <div className="card card-body">
+                        <ul>
+                            <li>
+                                <div className="row">
+                                    <div>Price</div>
+                                    <div className="price">{product.price} yen</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="row">
+                                    <div>Status</div>
+                                    <div>
+                                        {product.contInStock > 0 ? (
+                                            <span className="success">In Stock</span>
+                                        ) : (
+                                            <span className="danger">Unavailable</span>
+                                        )
+                                        }
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button className="primary block">Add to Cart</button>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
             </div>
-        
+
         </div>
-        </div>
-                
-    </div>
     )
 }
