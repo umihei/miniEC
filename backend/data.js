9683b8290da60424ff2da76146925d1dbd7fcff4
wakitani <wakitani@wakitanis-MacBook-Pro.local>
2021-09-18 11:21:25 +0900

    add express server, and nodemon

---
+++ backend/data.js
@@ -0,0 +1,76 @@
+const data = {
+    products: [{
+            _id: '1',
+            name: 'broccoli',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            contInStock: 10,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '2',
+            name: 'carrot',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            contInStock: 0,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '3',
+            name: 'onion',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            contInStock: 10,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '4',
+            name: 'tomato',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            contInStock: 10,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '5',
+            name: 'potato',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+        {
+            _id: '6',
+            name: 'water melon',
+            category: 'vegetable',
+            image: '/images/p1.jpg',
+            price: 680,
+            contInStock: 10,
+            brand: 'hankyu food market',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'fresh broccoli'
+        },
+    ]
+};
+
+export default data;
